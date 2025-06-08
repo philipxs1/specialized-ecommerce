22930602bc25b602f481fd4d839af3d2ebc2a796
Kyle Lambert <49013078+kyle-lambert@users.noreply.github.com>
2025-06-08 23:18:32 +0930

    Add loader test stuff

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function Layout() {
   return (
     <div className="flex h-full flex-col">
-      <div className="bg-amber-300 p-2">header</div>
+      <div className="bg-orange-600 p-2">header</div>
       <main className="flex-1">
         <Outlet />
       </main>
--- app/routes/shop/bikes/index.tsx
+++ app/routes/shop/bikes/index.tsx
@@ -1,3 +1,16 @@
-export default function BikesRoute() {
-  return <div>Bikes</div>;
+import type { Route } from ".react-router/types/app/routes/shop/bikes/+types";
+
+export async function loader() {
+  return {
+    message: "Phil",
+  };
+}
+
+export default function BikesRoute({ loaderData }: Route.ComponentProps) {
+  return (
+    <div>
+      Bikes
+      <div>{JSON.stringify(loaderData, null, 2)}</div>
+    </div>
+  );
 }
